dbf378ce7b993c580227892aa0db8efd9ad2bcfc
dhlopez <dhelaman@hotmail.com>
2017-02-27 13:48:17 -0500

    first commit

---
+++ PTLevelApp/PTLevelApp.UWP/BaseUrl.cs
@@ -0,0 +1,14 @@
+﻿using PTLevelApp.UWP;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl))]
+namespace PTLevelApp.UWP
+{
+    public class BaseUrl : IBaseUrl
+    {
+        public string Get()
+        {
+            return "ms-appx-web:///";
+        }
+    }
+}
---
+++ PTLevelApp/PTLevelApp/App.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PTLevelApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var nav = new NavigationPage(new Home());
+            MainPage = nav;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
